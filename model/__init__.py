e11b217a5539e910124dc230bc23563af56be955
Carl <83419286+nlp-enthusiast@users.noreply.github.com>
2023-11-08 20:12:27 +0800

    Delete model directory

--- model/__init__.py
+++
@@ -1,5 +0,0 @@
-from .chatglm2.modeling_chatglm import ChatGLMForConditionalGeneration as ChatGLM2ForConditionalGeneration
-from .chatglm2.tokenization_chatglm import ChatGLMTokenizer as ChatGLM2Tokenizer
-from .chatglm2.configuration_chatglm import ChatGLMConfig as ChatGLM2Config
-
-ModelMode={"glm2": {"model": ChatGLM2ForConditionalGeneration, "tokenizer": ChatGLM2Tokenizer, "config": ChatGLM2Config}}
